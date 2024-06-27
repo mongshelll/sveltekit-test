138a4ca42b8fc9cc0d37a069cb89ec4b804616c4
ljs1898 <ljs1898@eilab.kr>
2024-06-27 12:17:31 +0900

    240627 경로 수정

--- output/client/_app/immutable/entry/start.BmQlF3pe.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CA3iS8Xy.js";export{t as start};
--- output/server/manifest.js
+++ output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".nojekyll","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.BmQlF3pe.js","app":"_app/immutable/entry/app.BDCvPMFQ.js","imports":["_app/immutable/entry/start.BmQlF3pe.js","_app/immutable/chunks/entry.CA3iS8Xy.js","_app/immutable/chunks/scheduler.uGQJRBNU.js","_app/immutable/chunks/index.BeqEGQRZ.js","_app/immutable/entry/app.BDCvPMFQ.js","_app/immutable/chunks/scheduler.uGQJRBNU.js","_app/immutable/chunks/index.Dhsj7aeK.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.D-UKQv4U.js","app":"_app/immutable/entry/app.BEWJyBUq.js","imports":["_app/immutable/entry/start.D-UKQv4U.js","_app/immutable/chunks/entry.CUcGp8I-.js","_app/immutable/chunks/scheduler.uGQJRBNU.js","_app/immutable/chunks/index.BeqEGQRZ.js","_app/immutable/entry/app.BEWJyBUq.js","_app/immutable/chunks/scheduler.uGQJRBNU.js","_app/immutable/chunks/index.Dhsj7aeK.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
